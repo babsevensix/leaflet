28c833372299dbff28059b89ad3283ac803fac8e
Alberto <babsevensix@gmail.com>
2021-11-03 19:21:41 +0100

    Passo 1 - Schema layotu

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { LeftBarComponent } from './left-bar/left-bar.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { LeafletMapDirective } from './leaflet-map.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LeftBarComponent,
+    TopBarComponent,
+    FooterComponent,
+    LeafletMapDirective
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/leaflet-map.directive.spec.ts
@@ -0,0 +1,8 @@
+import { LeafletMapDirective } from './leaflet-map.directive';
+
+describe('LeafletMapDirective', () => {
+  it('should create an instance', () => {
+    const directive = new LeafletMapDirective();
+    expect(directive).toBeTruthy();
+  });
+});
