c69f29a45a47b61eaf0d7d9cb393cdf14e1b7c89
Alberto <babsevensix@gmail.com>
2021-11-03 19:27:35 +0100

    Aggiunta mappa alla direttiva e stili

--- src/app/leaflet-map.directive.ts
+++ src/app/leaflet-map.directive.ts
@@ -1,10 +1,19 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Directive({
   selector: '[appLeafletMap]'
 })
-export class LeafletMapDirective {
+export class LeafletMapDirective implements OnInit {
 
-  constructor() { }
+  private mappa!: L.Map ;
+
+  constructor(private elementRef: ElementRef) { }
+  ngOnInit(): void {
+    this.mappa = L.map(this.elementRef.nativeElement).setView([41.934441, 12.493692], 5);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'edupala.com © Angular LeafLet',
+    }).addTo(this.mappa);
+  }
 
 }
