8dafd8f3f554903b72ca758f948675a2f1adc9c7
Alberto <babsevensix@gmail.com>
2021-11-04 10:15:08 +0100

    Aggiunta marker

--- src/app/leaflet-map.directive.ts
+++ src/app/leaflet-map.directive.ts
@@ -1,19 +1,44 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 
 @Directive({
   selector: '[appLeafletMap]'
 })
 export class LeafletMapDirective implements OnInit {
+  _latlngs: L.LatLng[] = [];
+  _markers: L.Marker[] = [];
 
-  private mappa!: L.Map ;
-
-  constructor(private elementRef: ElementRef) { }
-  ngOnInit(): void {
-    this.mappa = L.map(this.elementRef.nativeElement).setView([41.934441, 12.493692], 5);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: 'edupala.com © Angular LeafLet',
-    }).addTo(this.mappa);
+  @Input() get latlngs() : L.LatLng[]
+  {
+    return this._latlngs;
+  }
+  set latlngs(value : L.LatLng[]){
+    this._latlngs = value;
+    console.log('setting latlngs');
+    this.setMarkers(this._latlngs);
   }
 
+  private mappa: L.Map |undefined;
+
+  constructor(private elementRef: ElementRef) { }
+
+  ngOnInit(): void {
+
+      this.mappa = L.map(this.elementRef.nativeElement).setView([41.934441, 12.493692], 5);
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'edupala.com © Angular LeafLet',
+      }).addTo(this.mappa);
+
+    this.setMarkers(this._latlngs);
+
+  }
+
+
+  private setMarkers(latlngs: L.LatLng[]) {
+    if (this.mappa ) {
+      this._markers = latlngs.map(latlng => {
+        return L.marker(latlng).addTo(<L.Map>this.mappa);
+      });
+    }
+  }
 }
